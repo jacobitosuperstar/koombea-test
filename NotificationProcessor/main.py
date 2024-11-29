69fe9c25ffd3854f39460510edc9a5c64dd5e0bc
Jacobo Mateo Bedoya Oquendo <mateo@Mac-mini-de-Jacobo.local>
2024-11-29 13:04:25 -0500

    FEAT: Error handling, Server Connection Issues.

--- NotificationProcessor/main.py
+++ NotificationProcessor/main.py
@@ -3,6 +3,17 @@ import sys
 import json
 import time
 import pika
+import requests
+from requests import Response
+
+
+def server_health_check()-> bool:
+    """Just checks if the server is up.
+    """
+    response: Response = requests.get("http://web:8000/ping")
+    if response.status_code != 200:
+        return False
+    return True
 
 def consumer(
     host: str,
@@ -36,7 +47,20 @@ def consumer(
         def callback(ch, method, properties, body):
             payload = json.loads(body)
             print(" [x] Received ")
-            print(payload)
+            print(f"Event {payload["event_id"]} has been send to {payload["user_id"]}: {payload["description"]}")
+
+            if not server_health_check():
+                print("Event status couldn't be updated.")
+                return
+            response: Response = requests.put(
+                f"http://web:8000/events/{payload["event_id"]}",
+                json={"status": "Processed"},
+            )
+            if response.status_code != 200:
+                print("There was some error with the update request.")
+                return
+            print("Status of the Event succesfully updated.")
+            return
 
         channel.basic_consume(
             queue=queue_name,
